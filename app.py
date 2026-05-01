11c32b13f821b75d93628df79e7c587f08f198d5
Abhay Jaiswal <abhayjaiswal789821@gmail.com>
2026-05-01 16:37:28 +0530

    First Commit: Python App & Dockerfile

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This grabs the ID of the Docker container
+    container_id = socket.gethostname()
+    return f"""
+    <h1>Zero-Touch Deployment Pipeline is ACTIVE! 🚀</h1>
+    <p>If you are seeing this, Jenkins successfully built and deployed the code.</p>
+    <p><b>Running inside Docker Container ID:</b> {container_id}</p>
+    """
+
+if __name__ == '__main__':
+    # Running on port 5000
+    app.run(host='0.0.0.0', port=5000)
